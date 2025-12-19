882aeecc9b0900e72941a334c367b96b01a6c96b
eg25035 <eg25035@edu.lu.lv>
2025-12-19 19:44:28 +0200

    initial commit

---
+++ eg25035_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from eg25035_libadd import *
+from eg25035_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
